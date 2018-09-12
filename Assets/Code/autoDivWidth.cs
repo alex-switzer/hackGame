83a4355d1fa8487f3268482ce586773add12a0b4
A J Switzer <15320@hillcrest-high.school.nz>
2018-09-12 15:10:16 +1200

    cool new stuff
    Signed-off-by: A J Switzer <15320@hillcrest-high.school.nz>

---
+++ Assets/Code/autoDivWidth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class autoDivWidth : MonoBehaviour {
+
+    public RectTransform parentRect;
+    public GridLayoutGroup gridLayout;
+
+	// Use this for initialization
+	void Start () {
+
+        float size = (parentRect.rect.width / gridLayout.constraintCount) - (gridLayout.spacing.x) + gridLayout.spacing.x/ gridLayout.constraintCount;
+
+        gridLayout.cellSize = new Vector2(size, gridLayout.cellSize.y);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
